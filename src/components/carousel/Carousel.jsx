9275da9cb8211d58c50e4d0fd8e218aca9e4cf1e
Bishal Kundu <bishalkundu17@gmail.com>
2023-07-21 18:00:16 +0530

    Added details page

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,36 +1,38 @@
-import React, {useRef} from "react"
+import React, { useRef } from "react"
 import {
     BsFillArrowLeftCircleFill,
     BsFillArrowRightCircleFill,
 } from "react-icons/bs"
-import {useNavigate} from "react-router-dom"
-import {useSelector} from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
 import dayjs from "dayjs"
 
 import ContentWrapper from "../contentWrapper/ContentWrapper"
 import Img from "../lazyLoadImage/Img"
 import PosterFallback from "../../assets/no-poster.png"
 import CircleRating from "../circleRating/CircleRating"
-
-import "./style.scss";
 import Genres from "../genres/Genres"
 
-const Carousel = ({data, loading, endpoint}) => {
+import "./style.scss"
+
+const Carousel = ({ data, loading, endpoint, title }) => {
     const carouselContainer = useRef()
-    const {url} = useSelector((state) => state.home)
-    const navigate = useNavigate
+    const { url } = useSelector((state) => state.home)
+    const navigate = useNavigate()
 
     const navigation = (dir) => {
         const container = carouselContainer.current
-        const scrollAmount = 
-            (dir === "left")
-                ? container.scrollLeft - (container.offsetWidth + 20) 
-                : container.scrollLeft + (container.offsetWidth + 20) 
+
+        const scrollAmount =
+            dir === "left"
+                ? container.scrollLeft - (container.offsetWidth + 20)
+                : container.scrollLeft + (container.offsetWidth + 20)
+
         container.scrollTo({
             left: scrollAmount,
-            behavior: "smooth"
-        })
-    }
+            behavior: "smooth",
+        });
+    };
 
     const skItem = () => {
         return (
@@ -41,13 +43,13 @@ const Carousel = ({data, loading, endpoint}) => {
                     <div className="date skeleton"></div>
                 </div>
             </div>
-        )
-    }
+        );
+    };
 
     return (
         <div className="carousel">
             <ContentWrapper>
-                {/* {title && <div className="carouselTitle">{title}</div>} */}
+                {title && <div className="carouselTitle">{title}</div>}
                 <BsFillArrowLeftCircleFill
                     className="carouselLeftNav arrow"
                     onClick={() => navigation("left")}
@@ -56,32 +58,33 @@ const Carousel = ({data, loading, endpoint}) => {
                     className="carouselRightNav arrow"
                     onClick={() => navigation("right")}
                 />
-                {!loading? (
+                {!loading ? (
                     <div className="carouselItems" ref={carouselContainer}>
                         {data?.map((item) => {
-                            const posterUrl = item.poster_path? 
-                                        url.poster + item.poster_path : 
-                                        PosterFallback
+                            const posterUrl = item.poster_path
+                                ? url.poster + item.poster_path
+                                : PosterFallback;
                             return (
-                                <div 
-                                    key={item.id} 
+                                <div
+                                    key={item.id}
                                     className="carouselItem"
-                                    onClick={() => navigate(`/${item.media_type || endpoint}/${item.id}`)}>
-                                        <div className="posterBlock">
-                                            <Img src={posterUrl}/>
-                                            <CircleRating rating={item.vote_average.toFixed(1)} />
-                                            <Genres data={item.genre_ids.slice(0, 2)}/>
-                                        </div>
-                                        <div className="textBlock">
-                                            <span className="title">
-                                                {item.title || item.name}
-                                            </span>
-                                            <span className="date">
-                                                {dayjs(item.release_Date).format("MMM D, YYYY")}
-                                            </span>
-                                        </div>
+                                    onClick={() => navigate(`/${item.media_type || endpoint}/${item.id}`)}
+                                >
+                                    <div className="posterBlock">
+                                        <Img src={posterUrl} />
+                                        <CircleRating rating={item.vote_average.toFixed(1)}/>
+                                        <Genres data={item.genre_ids.slice(0, 2)}/>
+                                    </div>
+                                    <div className="textBlock">
+                                        <span className="title">
+                                            {item.title || item.name}
+                                        </span>
+                                        <span className="date">
+                                            {dayjs(item.release_date || item.first_air_date).format("MMM D, YYYY")}
+                                        </span>
+                                    </div>
                                 </div>
-                            )
+                            );
                         })}
                     </div>
                 ) : (
@@ -95,7 +98,7 @@ const Carousel = ({data, loading, endpoint}) => {
                 )}
             </ContentWrapper>
         </div>
-    )
-}
+    );
+};
 
-export default Carousel
+export default Carousel;
